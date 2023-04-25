fbe3940a131a89132096b922c1d7acb645239847
melvinsibichen <melvinsibichen1020@gmail.com>
2023-04-25 14:15:27 +0530

    first commit

---
+++ src/main/java/com/example/melvindemo/MelvindemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.melvindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelvindemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelvindemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/melvindemo/MelvindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.melvindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelvindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
